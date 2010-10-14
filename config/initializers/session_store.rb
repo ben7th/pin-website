dad6a5b7bd7171f139f830ce7f3505b8edda6b40
fushang318 <fushang318@gmail.com>
2010-10-14 17:00:39 +0800

    增加配置文件

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,14 +2,11 @@
 
 # Your secret key for verifying cookie session data integrity.
 # If you change this key, all old sessions will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
+# Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.session = {
-  :key         => '_pin-website_session',
-  :secret      => 'a23c97bc0b688f17b85e9506eee4c5bae333ec76234a92d31e14a3ee092e7f6146d5c6eb8c529f32b20bace68b5de8bdeb19bf6dfeb903aaafdf7f1d97ab0527'
-}
 
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rake db:sessions:create")
-# ActionController::Base.session_store = :active_record_store
+  ActionController::Base.session = {
+    :domain => "2010.mindpin.com", 
+    :key=>'_mindpin_2010_session_dev',
+    :secret=>'883abe7844502ee307e376fa4d0509253d7f9e55fc8be69a934735cd470cc8671af39e27482885960f3364fa8af420b5519571193e22987c3e9e4f9da29f15fb'
+  }
