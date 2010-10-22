7b36da28e491cfd7aef289774d58fae775136dc9
fushang318 <fushang318@gmail.com>
2010-10-22 18:02:29 +0800

    评论完成

--- app/controllers/sidebar/web_site_infos_controller.rb
+++ app/controllers/sidebar/web_site_infos_controller.rb
@@ -3,9 +3,30 @@ class Sidebar::WebSiteInfosController < ApplicationController
   before_filter :pre_load
   layout nil
   def pre_load
-    @web_site = find_by_url(params[:url]) if params[:url]
+    @web_site = WebSite.find_by_domain(URI.parse(params[:url]).host) if params[:url]
   end
 
   def show;end
+
+  def show_detail
+    render :text=>"detail"
+  end
+
+  def comments
+    end
+
+  def comments_detail
+    render :text=>"comments_detail"
+  end
+
+  def comment
+    comment = @web_site.comments.new(params[:comment])
+    comment.creator = current_user
+    if comment.save
+      render_ui do |ui|
+        ui.mplist :insert,comment,:partial=>"sidebar/web_site_infos/parts/mpinfo_comment",:locals=>{:comment=>comment},:prev=>"TOP"
+      end
+    end
+  end
   
 end
