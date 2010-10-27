ab39759574e489b5ad5fed33e7c12d4a0f9cd36f
chinacheng <chinachengliwen@gmail.com>
2010-10-27 11:11:14 +0800

    关于历史记录排序

--- app/controllers/sidebar/browse_histories_infos_controller.rb
+++ app/controllers/sidebar/browse_histories_infos_controller.rb
@@ -4,6 +4,8 @@ class Sidebar::BrowseHistoriesInfosController < ApplicationController
   
   def index
     @browse_histories = current_user.browse_histories[0..9]
+    cookies[:browse_history_order] = params['order'] if params['order']
+    @order = cookies[:browse_history_order] || 'count'
   end
   
 end
